a8a8d87b50f5a503bafba0b7cc5bc5f9927959c1
Diego Turco <dhturco@gmail.com>
2020-07-21 22:32:15 -0300

    added bubble sort

---
+++ BubbleSort.js
@@ -0,0 +1,10 @@
+const array = [6, 4, 5, 3, 2, 1];
+for (let i = 0, size = array.length; i < array.length - 1; i++) {
+  for (let j = 0; j < size - 1 - i; j++) {
+    if (array[j] > array[j+1]) {
+      const tmp = array[j];
+      array[j] = array[j+1];
+      array[j+1] = tmp;
+    }
+  }
+}
