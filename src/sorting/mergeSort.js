9ffe7937c603b7e675f5428ac7c5bf0a050d856a
Diego Turco <dhturco@gmail.com>
2020-08-02 01:49:24 -0300

    added es6

--- src/sorting/mergeSort.js
+++ src/sorting/mergeSort.js
@@ -1,15 +1,4 @@
-const Compare = {
-  LESS_THAN: -1,
-  BIGGER_THAN: 1,
-  EQUALS: 0
-};
-
-function defaultCompare(a, b) {
-  if (a === b) {
-    return Compare.EQUALS;
-  }
-  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
-}
+import {Compare, defaultCompare} from "../util.js";
 
 function merge(left, right, compareFn) {
   let i = 0;
@@ -18,10 +7,10 @@ function merge(left, right, compareFn) {
   while (i < left.length && j < right.length) {
     result.push(compareFn(left[i], right[j]) === Compare.LESS_THAN ? left[i++] : right[j++]);
   }
-  let concat = result.concat(i < left.length ? left.slice(i) : right.slice(j));
-  return concat;
+  return result.concat(i < left.length ? left.slice(i) : right.slice(j));
 }
-function mergeSort(array, compareFn = defaultCompare) {
+
+export function mergeSort(array, compareFn = defaultCompare) {
   if (array.length > 1) {
     const { length } = array;
     const middle = Math.floor(length / 2);
